3554d499f52b955a7953f6336740d2537ee9b144
Masato Oshima <qqw75pn9@gmail.com>
2020-07-27 03:33:36 +0900

    Fix vpc settings of RDS

--- terraform/prod/rds.tf
+++ terraform/prod/rds.tf
@@ -6,9 +6,11 @@ resource "aws_db_instance" "db" {
   engine                       = "postgres"
   engine_version               = "12.3"
   instance_class               = "db.t2.micro"
+  name                         = "blog_${local.env}"
   username                     = "postgres"
-  password                     = "password" # !!! DO NOT USE production usecase
+  password                     = "password" # !!! DO NOT USE production usecase.
   parameter_group_name         = "default.postgres12"
+  db_subnet_group_name         = module.vpc.database_subnet_group
   vpc_security_group_ids       = [aws_security_group.db.id]
   copy_tags_to_snapshot        = true
   performance_insights_enabled = true
